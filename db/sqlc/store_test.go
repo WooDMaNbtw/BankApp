305e523f6a021404c7c25104144c6d457c49c0be
WooDMaNbtw <nikita.profatilov@gmail.com>
2024-09-18 00:27:55 +0400

    DB transaction lock, deadlocks in Go

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -12,14 +13,16 @@ func TestTransferTx(t *testing.T) {
 	account1 := createRandomAccount(t)
 	account2 := createRandomAccount(t)
 
+	fmt.Println(">>> before:", account1.Balance, account2.Balance)
+
 	// run a concurrent transfer transactions
-	n := 5
+	maxRetries := 5
 	amount := int64(10)
 
 	errs := make(chan error)
 	results := make(chan TransferTxResult)
 
-	for i := 0; i < n; i++ {
+	for retries := 0; retries < maxRetries; retries++ {
 		go func() {
 			result, err := store.TransferTx(context.Background(), TransferTxParams{
 				FromAccountID: account1.ID,
@@ -33,7 +36,8 @@ func TestTransferTx(t *testing.T) {
 	}
 
 	// checking results
-	for i := 0; i < n; i++ {
+	existed := make(map[int]bool)
+	for i := 0; i < maxRetries; i++ {
 		err := <-errs
 		require.NoError(t, err)
 
@@ -76,6 +80,37 @@ func TestTransferTx(t *testing.T) {
 		_, err = store.GetEntry(context.Background(), toEntry.ID)
 		require.NoError(t, err)
 
-		// TODO: check accounts' balance
+		// check accounts
+		fromAccount := result.FromAccount
+		require.NotEmpty(t, fromAccount)
+		require.Equal(t, account1.ID, fromAccount.ID)
+
+		toAccount := result.ToAccount
+		require.NotEmpty(t, toAccount)
+		require.Equal(t, account2.ID, toAccount.ID)
+		fmt.Println(">>> tx:", fromAccount.Balance, toAccount.Balance)
+		diff1 := account1.Balance - fromAccount.Balance
+		diff2 := toAccount.Balance - account2.Balance
+		require.Equal(t, diff1, diff2)
+		require.True(t, diff1 > 0)
+		require.True(t, diff1%amount == 0) // 1 * amount, 2 * amount ..., maxRetries * amount
+
+		k := int(diff1 / amount)
+
+		require.True(t, k >= 1 && k <= maxRetries)
+		require.NotContains(t, existed, k)
+		existed[k] = true
 	}
+
+	// check the final updated balances
+	updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	fmt.Println(">>> after:", updatedAccount1.Balance, updatedAccount2.Balance)
+
+	require.Equal(t, account1.Balance-(int64(maxRetries)*amount), updatedAccount1.Balance)
+	require.Equal(t, account2.Balance+(int64(maxRetries)*amount), updatedAccount2.Balance)
 }
